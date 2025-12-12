92fc036befa8aeb787b97b7cf9cd5a0d2da8ed31
balaji.rags <balaji.rags@gmail.com>
2025-12-12 17:24:33 +0530

    initialised spring boot project

---
+++ src/main/java/com/aifsd/guest_wifi_cards/GuestWifiCardsApplication.java
@@ -0,0 +1,13 @@
+package com.aifsd.guest_wifi_cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestWifiCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestWifiCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aifsd/guest_wifi_cards/GuestWifiCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aifsd.guest_wifi_cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestWifiCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
